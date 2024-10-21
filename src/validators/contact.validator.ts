8112c2c5ea21de1c9c1bf671cb93987f9f723b42
bastawesy94 <mohamedel.bastawesy2020@gmail.com>
2024-10-21 03:04:49 +0300

    updates

--- src/validators/contact.validator.ts
+++ src/validators/contact.validator.ts
@@ -3,13 +3,13 @@ import { body } from "express-validator";
 export const createContactValidator = [
   body("userKey").isString().notEmpty().withMessage("User Key is required"),
   body("phone").isString().notEmpty().withMessage("Phone number is required"),
-  body("address").isString().optional(), // Optional field
-  body("notes").isString().optional(), // Optional field
+  body("address").isString().optional(), 
+  body("notes").isString().optional(), 
 ];
 
 export const updateContactValidator = [
   body("phone").isString().optional(),
-  body("address").isString().optional(), // Optional field
-  body("notes").isString().optional(), // Optional field
+  body("address").isString().optional(),
+  body("notes").isString().optional(),
   body("userKey").isString().optional()
 ];
